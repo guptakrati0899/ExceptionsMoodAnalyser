0108ef19c33ae6c5ba1f8793507f02a6c2a849ae
KratiGupta <kratigupta8545@gmail.com>
2021-08-16 00:35:31 +0530

    Added Welcome message successfully

---
+++ main/java/com/exception/MoodAnalyser.java
@@ -0,0 +1,7 @@
+package com.exception;
+
+public class MoodAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to master branch of Exceptions-MoodAnalyser");
+    }
+}
